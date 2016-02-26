2e4e8ac6481b8152853ea069f33afe41e6a648da
Martyna Mizura <mixti87@gmail.com>
2016-02-26 05:58:42 +0100

    Redesign the hompepage to show a list of cars

--- src/CarambolaBundle/Controller/PageController.php
+++ src/CarambolaBundle/Controller/PageController.php
@@ -8,6 +8,18 @@ class PageController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CarambolaBundle:Page:index.html.twig');
+        $em = $this->getDoctrine()
+            ->getManager();
+
+        $cars = $em->createQueryBuilder()
+            ->select('c')
+            ->from('CarambolaBundle:Car',  'c')
+            ->addOrderBy('c.class', 'ASC')
+            ->getQuery()
+            ->getResult();
+
+        return $this->render('CarambolaBundle:Page:index.html.twig', array(
+            'cars' => $cars
+        ));
     }
 }
